62b219f3066aa2f4a8abb7051d4a8926a9c78e78
ElomariHana <elomarih5@gmail.com>
2025-07-20 16:46:10 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ElomariHana.github.io',
   plugins: [react(), tailwindcss()],
+  base: "/hana-elomari-portfolio",
 })
